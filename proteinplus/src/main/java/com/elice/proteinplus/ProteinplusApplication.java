58df1e079e10d93925c3da31f82ca0b545b663aa
[Cloud2] 정주용 <dusk3236@gmail.com>
2024-03-27 13:07:58 +0900

    initial-setup

---
+++ proteinplus/src/main/java/com/elice/proteinplus/ProteinplusApplication.java
@@ -0,0 +1,13 @@
+package com.elice.proteinplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProteinplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProteinplusApplication.class, args);
+	}
+
+}
---
+++ proteinplus/src/test/java/com/elice/proteinplus/ProteinplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.proteinplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProteinplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
