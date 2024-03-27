49ee0108439a9c2cb8d11ac7a2799de9d7c6ff62
[Cloud2] 정주용 <dusk3236@gmail.com>
2024-03-27 20:50:58 +0900

    feat: page test

---
+++ proteinplus/src/main/java/com/elice/proteinplus/category/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.elice.proteinplus.category.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping
+    public String test(){
+        return "frame";
+    }
+}
