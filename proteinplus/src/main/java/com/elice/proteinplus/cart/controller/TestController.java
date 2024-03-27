2b49456869f01eb7238c289f1539aae3e2ac60aa
[Cloud2] 정주용 <dusk3236@gmail.com>
2024-03-28 00:28:59 +0900

    feat: cart front page 추가

--- proteinplus/src/main/java/com/elice/proteinplus/cart/controller/TestController.java
+++ proteinplus/src/main/java/com/elice/proteinplus/cart/controller/TestController.java
@@ -1,4 +1,12 @@
 package com.elice.proteinplus.cart.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+    @GetMapping
+    public String test(){
+        return "cart/cart";
+    }
 }
