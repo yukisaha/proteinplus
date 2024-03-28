76c1be1e430786aa99148cab09a51b0133eeffef
seoyeonkang <seoyeonkang512@gmail.com>
2024-03-28 22:17:32 +0900

    Save changes before switching branches

---
+++ proteinplus/src/main/java/com/elice/proteinplus/TestController.java
@@ -0,0 +1,12 @@
+package com.elice.proteinplus;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//@Controller
+//public class TestController {
+//    @GetMapping(path = "/hello")
+//    public String hello() {
+//        return "order/order";
+//    }
+//}
