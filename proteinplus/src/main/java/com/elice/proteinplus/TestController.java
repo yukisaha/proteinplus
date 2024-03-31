e0613db83355268a0f20caa2e5dc1a479df69a10
seoyeonkang <seoyeonkang512@gmail.com>
2024-04-01 03:06:56 +0900

    feat: order front 작업

--- proteinplus/src/main/java/com/elice/proteinplus/TestController.java
+++ proteinplus/src/main/java/com/elice/proteinplus/TestController.java
@@ -3,10 +3,10 @@ package com.elice.proteinplus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//@Controller
-//public class TestController {
-//    @GetMapping(path = "/hello")
-//    public String hello() {
-//        return "order/order";
-//    }
-//}
+@Controller
+public class TestController {
+    @GetMapping(path = "/order")
+    public String hello() {
+        return "order/order";
+    }
+}
