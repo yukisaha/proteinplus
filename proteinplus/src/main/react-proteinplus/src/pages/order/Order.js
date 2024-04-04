d7f701e79592c05702a9f126079f3058acc01c47
seoyeonkang <seoyeonkang512@gmail.com>
2024-04-04 15:52:15 +0900

    feat: 주문 페이지 추가

--- proteinplus/src/main/react-proteinplus/src/pages/order/Order.js
+++ proteinplus/src/main/react-proteinplus/src/pages/order/Order.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import '../../res/css/order/order.css';
-import '../../res/css/frame.css';
+import '../../styles/order/css/Order.css';
+import Header from '../../components/Header'; // frame.js에서 Header 함수 import
+import Footer from '../../components/Footer'; // frame.js에서 Footer 함수 import
 
-export default function OrderPage(){
+
+export default function Order(){
     // function openUserDeliveryListPop(check) {
     //     $('body').css("overflow", "hidden");
     //
@@ -14,103 +16,9 @@ export default function OrderPage(){
     //     $('#userDeliveryListPop').load(deliveryListUrl);
     // }
 
-
-
     return (
         <div className="wrap main">
-
-            {/* ========== 공통 헤더 :: header ========== */}
-            <header id="header" className="header">
-                <div className="header-inner">
-                    <h1 className="logo "><a href="/"><span className="blind">ProteinPlus</span></a></h1>
-
-                    <div className="util">
-                        <ul>
-                            <li><a href="/auth/login">로그인</a></li>
-                            <li><a href="/member/join/general">회원가입</a></li>
-                            <li><a href="/mypage/orderlist">주문조회</a></li>
-                            <li><a href="/service/center">고객센터</a></li>
-                        </ul>
-                    </div>{/* util */}
-
-                    <div className="header-search">
-                        <div className="top-search">
-                            <input type="search" className="input-search" value="" maxLength="100"/>
-
-                                <button type="button" className="btn-top-search" id="btnTopSearch" name="btnTopSearch"
-                                        data-search-type="real" onClick="fnBtnTopSearch(this);">
-                                    <span className="blind">검색</span>
-                                </button>
-                        </div>
-                    </div>
-
-                    <div className="my-menu">
-                        <ul>
-                            <li><a href="" className="btn-util-pedometer" title=""><span className="blind">만보기</span></a></li>
-                            <li><a href="" className="btn-util-coupon" title=""><span className="blind">쿠폰</span></a></li>
-                            <li><a href="" className="btn-util-mypage" title=""><span className="blind">마이페이지</span></a>
-                            </li>
-                            <li>
-                                <a href="" className="btn-util-cart" title="">
-                                    <span className="blind">장바구니</span>
-                                    <em className="count">0</em>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>{/* my-menu */}
-                </div>
-                {/* header-inner */}
-
-                <div className="gnb-wrap">
-                    <div className="inner">
-                        <div className="category-wrap">
-                            <a href="#all-category" className="btn-menu-all"><span>카테고리</span></a>
-                        </div>
-                        <nav id="gnb" className="gnb">
-                            <ul>
-                                <li className="">
-                                    <a href="">
-                                        <span>랭킹</span>
-                                    </a>
-                                </li>
-                                <li className="">
-                                    <a href="">
-                                        <span>이달의 특가</span>
-                                    </a>
-                                </li>
-                                <li className="">
-                                    <a href="">
-                                        <span>브랜드관</span>
-                                    </a>
-                                </li>
-                                <li className="">
-                                    <a href="">
-                                        <span>1팩 담기</span>
-                                    </a>
-                                </li>
-                                <li className="">
-                                    <a href="">
-                                        <span>혜택정리</span>
-                                    </a>
-                                </li>
-                                <li className="">
-                                    <a href="">
-                                        <span>이벤트</span>
-                                    </a>
-                                </li>
-                                <li className="">
-                                    <a href="">
-                                        <span>..</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>{/*  gnb-wrap */}
-            </header>
-            {/* ========== 공통 헤더 :: header ========== */}
-
-
+            <Header />
             {/* ========== 컨텐츠 영역 :: container ========== */}
             <section id="contents" className="container">
                 <div className="content-wrap frame-sm">
@@ -118,7 +26,7 @@ export default function OrderPage(){
                         <h2 className="title-page">주문/결제</h2>
                     </div>
                     <div className="order-payment-area">
-                        <form id="ordFrm" name="ordFrm" action="/order/order" method="post" data-gtm-form-interact-id="0">
+                        <form id="ordFrm" name="ordFrm" action="/order/Order" method="post" data-gtm-form-interact-id="0">
                             <div className="order-info">
                                 <div className="list-head-sub">
                                     <h3 className="title-list">배송지 정보</h3>
@@ -358,64 +266,7 @@ export default function OrderPage(){
             </section>
 
             {/* ========== 컨텐츠 영역 :: container ========== */}
-
-
-            {/* ========== 공통 푸터 :: footer :: include.footer() 영역  ========== */}
-            <footer id="footer" className="footer">
-                <div className="footer-menu">
-                    <div className="frame-sm">
-                        <ul className="footer-menu-list">
-                            <li><a>회사소개</a></li>
-                            <li><a>공지사항</a></li>
-                            <li><a>입점 · 제휴 · 광고문의</a></li>
-                            <li><a>이용약관</a></li>
-                            <li><a className="text-black2 text-md">개인정보처리방침</a></li>
-                        </ul>{/* footer-menu-list */}
-                    </div>{/* footer-info-list */}
-                </div>{/* frame-sm */}
-                <div className="footer-info">
-                    <strong className="footer-logo"><span className="blind">ProteinPlus</span></strong>
-                    <div className="colum-info">
-                        <h5 className="blind">회사 정보</h5>
-                        <ul className="footer-biz-info">
-                            <li><span className="company-name">(주)푸드나무</span></li>
-                            <li>대표 : 김영문</li>
-                            <li>주소 : 서울특별시 마포구 월드컵북로 396, 15층(상암동, 누리꿈스퀘어 비지니스타워)</li>
-                            <li>사업자등록번호 : 105-87-81968
-                                사업자정보 확인
-                            </li>
-                            <li>통신판매업신고번호 : 제2015-서울마포-1058호</li>
-                            <li>개인정보보호책임자 : 이재길</li>
-                        </ul>
-                        <p className="copyright">Copyright©ProteinPlus All rights reserved.</p>
-                    </div>{/* colum */}
-                    <div className="colum-info">
-                        <ul className="customer-info">
-                            <li className="cscenter">고객센터 <em className="footer-tel">02-6405-8088</em></li>
-                            <li>FAX. 02-6351-8088</li>
-                            <li>E-mail. admin@foodnamoo.com</li>
-                        </ul>
-                        <div className="customer-btns">
-                            <a href="#"><span>고객의 제안</span></a>
-                            <a href="#"><span>1:1 문의</span></a>
-                        </div>
-                        <ul className="footer-sns-list">
-                            <li><a target="_blank"><i className="ico-sns-facebook"></i><span className="sns_blind">페이스북</span></a>
-                            </li>
-                            <li><a target="_blank"><i className="ico-sns-instagram"></i><span className="sns_blind">인스타그램</span></a>
-                            </li>
-                            <li><a target="_blank"><i className="ico-sns-blog"></i><span className="sns_blind">네이버블로그</span></a>
-                            </li>
-                            <li><a target="_blank"><i className="ico-sns-post"></i><span className="sns_blind">네이버포스트</span></a>
-                            </li>
-                            <li><a target="_blank"><i className="ico-sns-kakaotalk"></i><span className="sns_blind">카카오톡</span></a>
-                            </li>
-                            <li><a target="_blank"><i className="ico-sns-youtube"></i><span className="sns_blind">유튜브</span></a>
-                            </li>
-                        </ul>
-                    </div>{/* colum */}
-                </div>{/* footer-info */}
-            </footer>
+            <Footer />
         </div>
     );
 };
