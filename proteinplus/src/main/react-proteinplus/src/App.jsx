c149e1fa53e385fa17a45b5348de3ac546188627
seoyeonkang <seoyeonkang512@gmail.com>
2024-04-04 15:50:09 +0900

    feat: router 설정

--- proteinplus/src/main/react-proteinplus/src/App.jsx
+++ proteinplus/src/main/react-proteinplus/src/App.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 
-import OrderPage from './component/order/order';
-import CancelDetailPage from "./component/order/cancelDetail";
-import OrderDetailPage from "./component/order/orderDetail";
+import Home from './pages/home/Home.js';
+// import Cart from './pages/cart/Cart.js';
+import MypageFrame from './components/MypageFrame.js';
+// import WishList from './pages/cart/WishList.js';
+import Order from './pages/order/Order.js'
+import OrderDetail from './pages/order/OrderDetail.js'
+import CancelDetail from './pages/order/CancelDetail.js'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    },
+    // {
+    //     path: "/cart",
+    //     element: <Cart />
+    // },
+    {
+        path: "/mypageFrame",
+        element: <MypageFrame />
+    },
+    // {
+    //     path: "/wishList",
+    //     element: <WishList />
+    // },
+    {
+        path: "/order",
+        element: <Order />
+    },
+    {
+        path: "/orderDetail",
+        element: <OrderDetail />
+    },
+    {
+        path: "/cancelDetail",
+        element: <CancelDetail />
+    }
+
+]);
+
 export default function App(){
-    return(
-        <div className="App">
-            {/*<OrderPage/>*/}
-            {/*<CancelDetailPage/>*/}
-            <OrderDetailPage/>
-        </div>
+    return (
+        <RouterProvider router={router} />
     );
 }
