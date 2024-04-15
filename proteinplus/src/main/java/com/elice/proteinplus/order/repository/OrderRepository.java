5d27d04204cba1f79533611fa3fbed0274d9af8b
seoyeonkang <seoyeonkang512@gmail.com>
2024-04-15 17:30:26 +0900

    주석

--- proteinplus/src/main/java/com/elice/proteinplus/order/repository/OrderRepository.java
+++ proteinplus/src/main/java/com/elice/proteinplus/order/repository/OrderRepository.java
@@ -14,16 +14,16 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    //모든 주문 상태 주문 목록 조회 (관리자)
-    @Query("SELECT o FROM Order o WHERE o.user.userId = :userId ORDER BY o.orderDate desc")
-    List<Order> findByUserIdOrders(@Param("user_id") Long userId, Pageable pageable);
-
-    // 주문 상태가 CANCEL인 주문 목록 조회 (회원)
-    @Query("SELECT o FROM Order o WHERE o.user.userId = :userId AND o.orderStatus = 'CANCEL' ORDER BY o.orderDate desc")
-    List<Order> findByUserIdCancelList(@Param("user_id") Long userId, Pageable pageable);
-
-    // 주문 상태가 CANCEL이 아닌 주문 목록 조회 (회원)
-    @Query("SELECT o FROM Order o WHERE o.user.userId = :userId AND o.orderStatus != 'CANCEL' ORDER BY o.orderDate desc")
-    List<Order> findByUserIdOrderList(@Param("user_id") Long userId, Pageable pageable);
+//    //모든 주문 상태 주문 목록 조회 (관리자)
+//    @Query("SELECT o FROM Order o WHERE o.user.userId = :userId ORDER BY o.orderDate desc")
+//    List<Order> findByUserIdOrders(@Param("user_id") Long userId, Pageable pageable);
+//
+//    // 주문 상태가 CANCEL인 주문 목록 조회 (회원)
+//    @Query("SELECT o FROM Order o WHERE o.user.userId = :userId AND o.orderStatus = 'CANCEL' ORDER BY o.orderDate desc")
+//    List<Order> findByUserIdCancelList(@Param("user_id") Long userId, Pageable pageable);
+//
+//    // 주문 상태가 CANCEL이 아닌 주문 목록 조회 (회원)
+//    @Query("SELECT o FROM Order o WHERE o.user.userId = :userId AND o.orderStatus != 'CANCEL' ORDER BY o.orderDate desc")
+//    List<Order> findByUserIdOrderList(@Param("user_id") Long userId, Pageable pageable);
 
 }
