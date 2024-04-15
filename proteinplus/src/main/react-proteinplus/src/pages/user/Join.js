a32c049147dfc1e4d8292c2d643278d96a750ddd
yukisaha <0412judy@gmail.com>
2024-04-15 20:59:22 +0900

    feat: 회원가입 시 필요한 정보 추가

--- proteinplus/src/main/react-proteinplus/src/pages/user/Join.js
+++ proteinplus/src/main/react-proteinplus/src/pages/user/Join.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import "../../styles/user/css/join.scoped.css"
@@ -15,29 +15,76 @@ function Join(){
     const [loginId, setLoginId] = useState("");
     const [loginPwd, setLoginPwd] = useState("");
     const [loginPwdCheck, setLoginPwdCheck] = useState("");
+    const [name, setName] = useState("");
+    const [nickName, setNickName] = useState("");
+    const [phone, setPhone] = useState("");
     const [email, setEmail] = useState("");
 
     const [isIdAvailable, setIsIdAvailable] = useState(false);
+    const [idMessage, setIdMessage] = useState("");
+    const [pwdMessage, setPwdMessage] = useState("");
+    const [idDuplicateStyle, setIdDuplicateStyle] = useState('');
+    const [pwdDuplicateStyle, setPwdDuplicateStyle] = useState('');
 
     const navigate = useNavigate();
 
+    const handleNameChange = (e) => {
+        const name = e.target.value;
+        setName(name);
+    };
+    const handleNickNameChange = (e) => {
+        const nickName = e.target.value;
+        setNickName(nickName);
+    };
     const handleLoginIdChange = (e) => {
         const LoginId = e.target.value;
         setLoginId(LoginId);
         setIsIdAvailable(false); //아이디 변경 시 다시 중복체 크 해야함
-    }
+        setIdMessage("");
+    };
     const handleLoginPwdChange = (e) => {
         const LoginPwd = e.target.value;
         setLoginPwd(LoginPwd);
-    }
+    };
     const handleLoginPwdCheckChange = (e) => {
         const LoginPwdCheck = e.target.value;
         setLoginPwdCheck(LoginPwdCheck);
-    }
+    };
+
+    useEffect(() => {
+        setPwdMessage("");
+        if(loginPwd){
+            if(loginPwdCheck === loginPwd){
+                setPwdMessage("비밀번호가 일치합니다");
+                setPwdDuplicateStyle(`idAvailable`);
+            }else{
+                setPwdMessage("비밀번호가 일치하지 않습니다")
+                setPwdDuplicateStyle(`idUnavailable`);
+            }
+        }
+    }, [loginPwd, loginPwdCheck])
+
+    const handlePhoneChange = (e) => {
+        const numbersOnly = e.target.value.replace(/\D/g, "");
+        if (numbersOnly.length <= 11) {
+            setPhone(numbersOnly);
+        }
+    };
+
+    const displayFormattedPhoneNumber = (numbers) => {
+        if (numbers.length <= 3) {
+            return numbers;
+        } else if (numbers.length <= 7) {
+            return `${numbers.slice(0, 3)}-${numbers.slice(3)}`;
+        } else {
+            return `${numbers.slice(0, 3)}-${numbers.slice(3, 7)}-${numbers.slice(7)}`;
+        }
+    };
+
     const handleEmailChange = (e) => {
         const email = e.target.value;
         setEmail(email);
-    }
+    };
 
     const idDuplicateCheck = async (e) => {
         e.preventDefault();
@@ -54,16 +101,21 @@ function Join(){
             const response = await axios.get(`${Spring_Server_Ip}/member/join/idDuplicateCheck`, {params: data});
             //이미 존재하는 아이디일 경우
             if(response.data === true) {
-                alert("이미 사용 중인 아이디입니다.");
+                setIdMessage("이미 사용 중인 아이디입니다.");
+                setIsIdAvailable(false);
+                setIdDuplicateStyle(`idUnavailable`);
             }else{
-                alert("사용할 수 있는 아이디입니다.");
+                setIdMessage("사용할 수 있는 아이디입니다.");
                 //아이디 입력 필드 잠금
                 setIsIdAvailable(true);
+                setIdDuplicateStyle(`idAvailable`);
             }
         } catch (error) {
             alert("에러 발생");
         }
-    }
+    };
+
+
 
     const emailDuplicateCheck = async () => {
         try {
@@ -77,13 +129,61 @@ function Join(){
             alert("에러 발생");
             return false;
         }
-    }
+    };
+
+    const phoneDuplicateCheck = async () => {
+        try {
+            const data = {
+                phone: phone
+            };
+            const response = await axios.get(`${Spring_Server_Ip}/member/join/phoneDuplicateCheck`, {params: data});
+            //이미 존재하는 전화번호일 경우
+            return response.data === true;
+        } catch (error) {
+            alert("에러 발생");
+            return false;
+        }
+    };
 
     const join = async (e) => {
         e.preventDefault();
+        if(!name){
+            alert("이름을 입력해주세요.");
+            return;
+        }
+
+        if(!nickName){
+            alert("닉네임을 입력해주세요.");
+            return;
+        }
+
+        if (!loginId) {
+            alert("아이디를 입력해주세요.");
+            return;
+        }
 
         if(!isIdAvailable){
             alert("아이디 중복 확인을 해주세요")
+            return;
+        }
+
+        if (!loginPwd) {
+            alert("비밀번호 입력해주세요.");
+            return;
+        }
+
+        if (!loginPwdCheck) {
+            alert("비밀번호 확인을 해주세요.");
+            return;
+        }
+
+        if (!phone) {
+            alert("전화번호를 입력해주세요.");
+            return;
+        }
+
+        if (!email) {
+            alert("이메일을 입력해주세요.");
             return;
         }
 
@@ -92,17 +192,25 @@ function Join(){
 
             //이메일 중복 확인
             const isEmailDuplicate = await emailDuplicateCheck();
+            const isPhoneDuplicate = await phoneDuplicateCheck();
             if(!isEmailDuplicate){
-                try {
-                    const data = {
-                        loginId: loginId,
-                        loginPwd: loginPwd,
-                        email: email
-                    };
-                    await axios.post(`${Spring_Server_Ip}/member/join`, null, {params: data});
-                    navigate('/member/join/complete', {state: {loginId: loginId}}, {replace: true});
-                } catch (error) {
-                    alert("회원가입에 실패했습니다.");
+                if(!isPhoneDuplicate){
+                    try {
+                        const data = {
+                            name: name,
+                            nickname: nickName,
+                            loginId: loginId,
+                            loginPwd: loginPwd,
+                            phone: phone,
+                            email: email
+                        };
+                        await axios.post(`${Spring_Server_Ip}/member/join`, null, {params: data});
+                        navigate('/member/join/complete', {state: {loginId: loginId}}, {replace: true});
+                    } catch (error) {
+                        alert("회원가입에 실패했습니다.");
+                    }
+                }else{
+                    alert("해당 전화번호로 가입된 계정이 있습니다");
                 }
             }else{
                 alert("해당 이메일로 가입된 계정이 있습니다");
@@ -111,7 +219,7 @@ function Join(){
             setLoginPwdCheck("");
             alert("비밀번호가 일치하지 않습니다");
         }
-    }
+    };
 
     return(
         <div className="wrap main">
@@ -127,6 +235,38 @@ function Join(){
                         <form id="join_form" className="join_form" onSubmit={join}>
                             {/* input submit */}
                             <fieldset>
+                                <div className="join_input-group user_w-full">
+                                    <label htmlFor="name"
+                                    >이름 <span id="required-text">[필수]</span></label
+                                    >
+                                    <input
+                                        id="name"
+                                        name="name"
+                                        className="join_input-text type-lg"
+                                        type="name"
+                                        onChange={handleNameChange}
+                                        value={name}
+                                        placeholder="이름"
+                                        maxLength="12"
+                                        required
+                                    />
+                                </div>
+                                <div className="join_input-group user_w-full">
+                                    <label htmlFor="name"
+                                    >닉네임 <span id="required-text">[필수]</span></label
+                                    >
+                                    <input
+                                        id="nickname"
+                                        name="nickname"
+                                        className="join_input-text type-lg"
+                                        type="nickname"
+                                        onChange={handleNickNameChange}
+                                        value={nickName}
+                                        placeholder="닉네임"
+                                        maxLength="12"
+                                        required
+                                    />
+                                </div>
                                 <div className="join_input-group user_w-full">
                                     <label htmlFor="id"
                                     >아이디 <span id="required-text">[필수]</span>
@@ -144,9 +284,8 @@ function Join(){
                                             required
                                         />
                                         <button onClick={idDuplicateCheck}>중복 확인</button>
-                                        {/*중목체크가 되었을경우*/}
-                                        {/*<div className ={`idDuplicateCheck`}>사용할 수 있는 아이디입니다</div>*/}
-                                    </div>
+                                      </div>
+                                    <div className={`${idDuplicateStyle}`}>{idMessage}</div>
                                 </div>
                                 <div className="join_input-group user_w-full">
                                     <label htmlFor="pw"
@@ -174,12 +313,21 @@ function Join(){
                                         maxLength="100"
                                         required
                                     />
-
-                                    {/*비밀번호 확인이 비밀번호에 입력한 값과 동일할 경우 */}
-                                    {/*<div>비밀번호가 일치합니다</div>*/}
-                                    {/*동일하지 않을 경우*/}
-                                    {/*<div>비밀번호가 일치하지 않습니다</div>*/}
-
+                                    <div className={`${pwdDuplicateStyle}`}>{pwdMessage}</div>
+                                </div>
+                                <div className="join_input-group user_w-full">
+                                    <label htmlFor="phone">전화번호 <span id="required-text">[필수]</span></label>
+                                    <input
+                                        id="phone"
+                                        name="phone"
+                                        className="join_input-text type-lg phone"
+                                        type='text'
+                                        onChange={handlePhoneChange}
+                                        value={displayFormattedPhoneNumber(phone)}
+                                        placeholder="01012345678"
+                                        maxLength="13"
+                                        required
+                                    />
                                 </div>
                                 <div className="join_input-group user_w-full">
                                     <label htmlFor="email"
