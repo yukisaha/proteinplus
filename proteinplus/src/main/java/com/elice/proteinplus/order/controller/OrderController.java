d744f62ae0f5ba26870872c51ab99a0c9d4134ad
seoyeonkang <seoyeonkang512@gmail.com>
2024-04-16 14:54:12 +0900

    feat: cartDto 배송정보 받아오기

--- proteinplus/src/main/java/com/elice/proteinplus/order/controller/OrderController.java
+++ proteinplus/src/main/java/com/elice/proteinplus/order/controller/OrderController.java
@@ -52,12 +52,8 @@ public class OrderController {
         log.info("cartDto ------------------------n" + cartDto.toString());
 
     }
-    @GetMapping("/order")
-    public List<Pro> getorder() {
 
-    }
-
-    // 주문의 배송 정보를 업데이트합니다.
+    // 주문의 배송 정보를 수정합니다.
     @PutMapping("/user/mypage/orderlist/{orderId}/edit")
     public ResponseEntity<Void> updateDeliveryInfo(@PathVariable Long orderId,
                                                    @RequestBody DeliveryDto deliveryDto) {
