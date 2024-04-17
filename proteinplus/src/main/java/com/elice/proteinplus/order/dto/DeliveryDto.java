a3ab84ee3cf12ab9e7ed4dd2859306227784e35a
seoyeonkang <seoyeonkang512@gmail.com>
2024-04-17 19:19:16 +0900

    feat: 주문 생성 기능 수정

--- proteinplus/src/main/java/com/elice/proteinplus/order/dto/DeliveryDto.java
+++ proteinplus/src/main/java/com/elice/proteinplus/order/dto/DeliveryDto.java
@@ -9,6 +9,9 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class DeliveryDto {
+
+    private Long orderId;
+
     private String receiverName;
 
     private String receiverPhoneNumber;
