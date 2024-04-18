73f067b321b7e85f02617c36f21ff649473c9b8a
seoyeonkang <seoyeonkang512@gmail.com>
2024-04-18 20:55:41 +0900

    fix: 주문 내역 오류 수정

--- proteinplus/src/main/java/com/elice/proteinplus/order/repository/OrderRepository.java
+++ proteinplus/src/main/java/com/elice/proteinplus/order/repository/OrderRepository.java
@@ -16,14 +16,14 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     //모든 주문 상태 주문 목록 조회 (관리자)
     @Query("SELECT o FROM Order o WHERE o.user.id = :userId ORDER BY o.orderDate desc")
-    List<Order> findByUserId(Long userId, Pageable pageable);
+    List<Order> findByUserId(@Param("userId") Long userId, Pageable pageable);
 
     // 주문 상태가 CANCEL인 주문 목록 조회 (회원)
     @Query("SELECT o FROM Order o WHERE o.user.id = :userId AND o.orderStatus = 'CANCEL' ORDER BY o.orderDate desc")
-    List<Order> findByUserIdCancelList(Long userId, Pageable pageable);
+    List<Order> findByUserIdCancelList(@Param("userId") Long userId, Pageable pageable);
 
     // 주문 상태가 CANCEL이 아닌 주문 목록 조회 (회원)
     @Query("SELECT o FROM Order o WHERE o.user.id = :userId AND o.orderStatus != 'CANCEL' ORDER BY o.orderDate desc")
-    List<Order> findByUserIdOrderList(Long userId, Pageable pageable);
+    List<Order> findByUserIdOrderList(@Param("userId") Long userId, Pageable pageable);
 
 }
